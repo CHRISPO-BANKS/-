2964e20ae564283f49a1f8e12e97a2a9d60f6c15
CHRISPO-BANKS <ekemejerochristian@gmail.com>
2025-03-16 02:32:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "𝙲𝙷𝚁𝙸𝚂𝙿𝙾 🥷",
+  botOwner: '𝙲𝙷𝚁𝙸𝚂𝙿𝙾 𝙱𝙰𝙽𝙺𝚂 💱 𓃵⁩⁩ 𓃵⁩⁩', // Your name
+  ownerNumber: '2349011774883', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
